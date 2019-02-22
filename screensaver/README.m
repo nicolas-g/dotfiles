65c08c00e8f3a1ddf3688833366055f29056adcf
Nicolas G <nicolasg@gmail.com>
2019-02-22 17:10:14 -0500

    add screen saver file

---
+++ screensaver/README.m
@@ -0,0 +1,17 @@
+
+### RedPill Matix
+https://github.com/lpar/RedPill/
+
+Download Link
+http://1drv.ms/1pvSPLr
+
+RedPill - Matrix Screen Saver for Mac OS Mavericks 10.9
+Installation Instructions
+* Download the screen saver from the link
+* Unzip it and you'll find RedPill.saver
+* Double click on it
+* System Preferences will pop up where it will ask you to install it
+* Go ahead and install it
+* Choose "RedPill" from the list of installed screen savers. Mostly It will be shown in last.
+* For configuring the speed and various other options, you can click on Screen Saver Options right below the screen saver preview window.
+* By default it will work on all the monitors attached to your Mac but that can be modified in Screen Saver Options.
