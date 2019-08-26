1c5cd56c56c55d838d99a0ca0b4d2b4583b6953a
Nicolas G <nicolasg@gmail.com>
2019-08-25 21:32:59 -0400

    fix backup history bugs

--- roles/dotfiles/files/ng_backup_history.sh
+++ roles/dotfiles/files/ng_backup_history.sh
@@ -6,8 +6,8 @@
 BACKUP_DIR=/keybase/private/nicolasg7/backups
 
 [ -d $BACKUP_DIR/backup.daily ] || mkdir $BACKUP_DIR/backup.daily
-[ -d $BACKUP_DIR/backup.weekly] || mkdir $BACKUP_DIR/backup.weekly
-[ -d $BACKUP_DIR/backup.monthly] || mkdir $BACKUP_DIR/backup.monthly
+[ -d $BACKUP_DIR/backup.weekly ] || mkdir $BACKUP_DIR/backup.weekly
+[ -d $BACKUP_DIR/backup.monthly ] || mkdir $BACKUP_DIR/backup.monthly
 
 # Source folder where files are backed
 BACKUP_FILE=/Users/ngeorgakopoulos/.zsh_history
@@ -33,20 +33,20 @@ fi
 
   # On first month day do
 if [ "$month_day" -eq 1 ] ; then
-  destination=$BACKUP_DIR/backup.monthly/$date_daily
+  destination=$BACKUP_DIR/backup.monthly/
   else
   # On saturdays do
   if [ "$week_day" -eq 6 ] ; then
-  destination=$BACKUP_DIR/backup.weekly/$date_daily
+  destination=$BACKUP_DIR/backup.weekly/
   else
   # On any regular day do
-  destination=$BACKUP_DIR/backup.daily/$date_daily
+  destination=$BACKUP_DIR/backup.daily/
   fi
 fi
 
 # Copy the file to backup dir
-mkdir $destination
-cp -aR $BACKUP_FILE $destination
+# ${BACKUP_FILE##*/.} <= get only basename of the full path and remove leading `.`
+cp -aR $BACKUP_FILE $destination/${BACKUP_FILE##*/.}_$date_daily
 
 # daily - keep for 14 days
 find $BACKUP_DIR/backup.daily/ -maxdepth 1 -mtime +14 -type d -exec rm -rv {} \;
