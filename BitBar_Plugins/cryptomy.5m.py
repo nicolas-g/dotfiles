932d108050ffd498978fa8ba761d2dbd6ca8d1ef
Nicolas G <nicolasg@gmail.com>
2021-01-12 16:25:25 -0500

    adjust new price limit alert, add retry and color

--- BitBar_Plugins/cryptomy.5m.py
+++ BitBar_Plugins/cryptomy.5m.py
@@ -14,11 +14,12 @@
 #
 import subprocess
 
-BTC_PRICE_ALERT=36000
-ETH_PRICE_ALERT=1200
+BTC_PRICE_ALERT=37500
+ETH_PRICE_ALERT=1250
 BCH_PRICE_ALERT=550
 
-btc_cmd = "curl -s https://api.coinbase.com/v2/prices/BTC-USD/buy | /usr/local/bin/jq '.data.amount'"
+
+btc_cmd = "curl -s --retry 5 --retry-delay 15 https://api.coinbase.com/v2/prices/BTC-USD/buy | /usr/local/bin/jq '.data.amount'"
 btc_cmd_process= subprocess.Popen(btc_cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
 btc_stdout, btc_stderr = btc_cmd_process.communicate()
 btc_stdout=btc_stdout.decode("utf-8")
@@ -26,7 +27,7 @@ btc_stdout=btc_stdout.strip()
 btc_stdout=btc_stdout.replace('\"', '')
 btc=float(btc_stdout)
 
-eth_cmd = "curl -s https://api.coinbase.com/v2/prices/ETH-USD/buy | /usr/local/bin/jq '.data.amount'"
+eth_cmd = "curl -s --retry 5 --retry-delay 15 https://api.coinbase.com/v2/prices/ETH-USD/buy | /usr/local/bin/jq '.data.amount'"
 eth_cmd_process= subprocess.Popen(eth_cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
 eth_stdout, eth_stderr = eth_cmd_process.communicate()
 eth_stdout=eth_stdout.decode("utf-8")
@@ -34,7 +35,7 @@ eth_stdout=eth_stdout.strip()
 eth_stdout=eth_stdout.replace('\"', '')
 eth=float(eth_stdout)
 
-bch_cmd = "curl -s https://api.coinbase.com/v2/prices/BCH-USD/buy | /usr/local/bin/jq '.data.amount'"
+bch_cmd = "curl -s --retry 5 --retry-delay 15 https://api.coinbase.com/v2/prices/BCH-USD/buy | /usr/local/bin/jq '.data.amount'"
 bch_cmd_process= subprocess.Popen(bch_cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
 bch_stdout, bch_stderr = bch_cmd_process.communicate()
 bch_stdout=bch_stdout.decode("utf-8")
@@ -48,12 +49,12 @@ if eth >= ETH_PRICE_ALERT or btc >= BTC_PRICE_ALERT or bch >= BCH_PRICE_ALERT :
     stdout2, stderr2 = process2.communicate()
     print("B| color=red")
     print("---")
-    print("BTC: ", btc_stdout, "| href=https://coinmarketcap.com/currencies/bitcoin/")
-    print("ETH: ", eth_stdout, "| href=https://coinmarketcap.com/currencies/ethereum/")
-    print("BCH: ", bch_stdout, "| href=https://coinmarketcap.com/currencies/bitcoin-cash/")
+    print("BTC: ", btc_stdout, "| color=red href=https://coinmarketcap.com/currencies/bitcoin/")
+    print("ETH: ", eth_stdout, "| color=red href=https://coinmarketcap.com/currencies/ethereum/")
+    print("BCH: ", bch_stdout, "| color=red href=https://coinmarketcap.com/currencies/bitcoin-cash/")
 else:
     print("B| color=green")
     print("---")
-    print("BTC: ", btc_stdout, "| href=https://coinmarketcap.com/currencies/bitcoin/")
-    print("ETH: ", eth_stdout, "| href=https://coinmarketcap.com/currencies/ethereum/")
-    print("BCH: ", bch_stdout, "| href=https://coinmarketcap.com/currencies/bitcoin-cash/")
+    print("BTC: ", btc_stdout, "| color=green href=https://coinmarketcap.com/currencies/bitcoin/")
+    print("ETH: ", eth_stdout, "| color=green href=https://coinmarketcap.com/currencies/ethereum/")
+    print("BCH: ", bch_stdout, "| color=green href=https://coinmarketcap.com/currencies/bitcoin-cash/")
