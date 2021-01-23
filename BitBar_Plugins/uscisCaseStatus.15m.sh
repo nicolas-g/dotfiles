7838c59c1f085072628944c898cdb5fd293d89d7
Nicolas G <nicolasg@gmail.com>
2021-01-23 10:21:17 -0500

    BItbar uscisCaseStatus.15m.sh updates:
    - can now detect when site is unreachable
    - update md5sum hash with the latest case status (Jan22)
    - change NG logo to G

--- BitBar_Plugins/uscisCaseStatus.15m.sh
+++ BitBar_Plugins/uscisCaseStatus.15m.sh
@@ -11,7 +11,7 @@
 # and know when the latest case status hash is different in order to alert
 
 tmp_file="/tmp/uscis_lin_hash.txt"
-last_cases_status_hash="161ff8685665b31df1a3230e4b824ea4"
+last_cases_status_hash="f9121deed5eead4672c17c0d07107d7c"
 uscis_case_id="LIN1822851191"
 
 curl -s --retry 5 --retry-delay 15 -X POST -d "appReceiptNum=$uscis_case_id"  https://egov.uscis.gov/casestatus/mycasestatus.do | grep $uscis_case_id | grep $uscis_case_id > $tmp_file
@@ -21,11 +21,17 @@ last_update_date=$(grep On $tmp_file | cut -d"," -f 1 | cut -d">" -f 2 | awk '{ 
 current_case_status_hash=$(/usr/local/bin/md5sum $tmp_file | cut -d" " -f1)
 
 if [[ ${current_case_status_hash} == "${last_cases_status_hash}" ]]; then
-    echo "NG| color=green"
+    echo "G| color=green"
     echo "---"
     echo "Last case update: $last_update_date"
+elif [[ ${current_case_status_hash} == "d41d8cd98f00b204e9800998ecf8427e" ]]; then
+    # when site is not reachable the m5sum hash value is d41d8cd98f00b204e9800998ecf8427e
+    echo "G!| color=red"
+    echo "---"
+    echo "$last_update_date SITE NOT REACHABLE"
+    /usr/bin/say --rate 230 "Immigration site not reachable"
 else
-    echo "NG!| color=red"
+    echo "G| color=red"
     echo "---"
     echo "Last case update: $last_update_date"
     /usr/bin/say --rate 230 "Immigration status, updated"
