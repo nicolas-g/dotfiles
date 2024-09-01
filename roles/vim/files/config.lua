ca29cace739a74cc4869a8d68e35601e432ab8a1
Nicolas G. <2202932+nicolas-g@users.noreply.github.com>
2024-09-01 16:05:48 +0300

    enable spelling on Lunar Vim

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -30,3 +30,7 @@ local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
 pcall(function()
   require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
 end)
+
+# Enable Spelling
+vim.opt.spell = true
+vim.opt.spelllang = { "en" }
